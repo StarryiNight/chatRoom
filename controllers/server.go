69aedba8311ced0640e923779073378c3e634fc7
StarryiNight <78688426+StarryiNight@users.noreply.github.com>
2021-08-17 00:36:07 +0800

    Update server.go

--- controllers/server.go
+++ controllers/server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+//设置参数
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
@@ -17,15 +18,17 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+//服务器
 func Server(c *gin.Context) {
+	//获取进入的房间
 	roomid := c.Query("roomid")
-
 	wsConn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
+	//把信息传入新进入聊天室的通道 进行广播
 	con := &models.Connection{Send: make(chan []byte, 256), WsConn: wsConn}
 	msg := models.Message{nil, roomid, c.GetString("username"), con}
 
